d83b37a3baf7e097f55ec41fae645f1ce9fd14a0
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2022-02-27 19:30:34 +0000

    ADD App insights. ADD outputs.tf for FunctionApp apply. UPDATE tags.

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+# terraform/outputs.tf
+
+output "function_app_name" {
+  value       = azurerm_function_app.cete-function-app.name
+  description = "Deployed function app name"
+}
+
+output "function_app_default_hostname" {
+  value       = azurerm_function_app.cete-function-app.default_hostname
+  description = "Deployed function app hostname"
+}
