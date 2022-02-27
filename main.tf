19dcc6f99cca734ffbc95c7922a8a1c2ab98af64
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2022-02-27 03:12:36 +0000

    ADD storage accounts to infra. CLEANUP quality check.

--- main.tf
+++ main.tf
@@ -46,11 +46,35 @@ resource "azurerm_resource_group" "cete-stg-rg" {
   }
 }
 
+resource "azurerm_storage_account" "cete-stg-storage-account" {
+  name                     = "cete-stg-storage-account"
+  resource_group_name      = azurerm_resource_group.cete-stg-rg.name
+  location                 = azurerm_resource_group.cete-stg-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "Staging"
+  }
+}
+
 # LIVE
-resource "azurerm_resource_group" "cete-prod-rg" {
-  name     = "cete-prod-rg"
+resource "azurerm_resource_group" "cete-prd-rg" {
+  name     = "cete-prd-rg"
   location = "centralus"
   tags = {
     Environment = "Production"
+  }
+}
+
+resource "azurerm_storage_account" "cete-prd-storage-account" {
+  name                     = "cete-prd-storage-account"
+  resource_group_name      = azurerm_resource_group.cete-prd-rg.name
+  location                 = azurerm_resource_group.cete-prd-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "Production"
   }
 }
