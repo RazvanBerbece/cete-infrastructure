3d353d84bdb30ff01d1a916abf743639059eaf7b
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2022-03-02 00:29:19 +0000

    FIX Email address list - (14)

--- variables.tf
+++ variables.tf
@@ -31,6 +31,6 @@ variable "ARM_TENANT_ID" {
 
 variable "budget_admin_emails" {
   description = "Budget admin email address list for cost notifications"
-  type        = list(string)
+  type        = list
   sensitive   = true
 }
