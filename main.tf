378ddca9a02285c3e7b7cc44b4c41bed47ac26cf
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2022-03-03 22:10:21 +0000

    ADD ENV variables to TF Cloud. REMOVE secrets.tfvars build step from workflows. REMOVE secrets from GH.

--- main.tf
+++ main.tf
@@ -31,10 +31,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  tenant_id       = var.ARM_TENANT_ID
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
 }
 
 #################################### CREATE RESOURCES & BUDGET ####################################
--- variables.tf
+++ variables.tf
@@ -9,28 +9,33 @@ variable "ARM_CLIENT_ID" {
   description = "Client ID obtained from the Azure SP"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
 variable "ARM_CLIENT_SECRET" {
   description = "Client secret obtained from the Azure SP"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
 variable "ARM_SUBSCRIPTION_ID" {
   description = "Subscription ID obtained from the Azure SP"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
 variable "ARM_TENANT_ID" {
   description = "Tenant ID obtained from the Azure SP"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
 variable "BUDGET_ADMIN_EMAILS" {
   description = "Budget admin email address list for cost notifications"
   type        = list(string)
   sensitive   = true
+  default     = []
 }
